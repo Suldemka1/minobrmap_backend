eb75af0d766171643e7be5151f7e62e137020ce7
Suldemka1 <suldemka1@gmail.com>
2022-10-22 00:42:30 +0700

    dockerize #1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
-import {ServeStaticModule} from '@nestjs/serve-static'
-import { Map } from './data/data.model';
-import { DataModule } from './data/data.module';
 import { OrganizationsModule } from './organizations/organizations.module';
 import { OrganizationModel } from './organizations/models/organization.model';
 import 'dotenv/config';
-import {join} from 'path'
 import { PolygonsModule } from './polygons/polygons.module';
-import { PolygonsModel } from "./polygons/polygons.model";
+import { PolygonsModel } from './polygons/polygons.model';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client')
-    }),
     SequelizeModule.forRoot({
       dialect: 'mysql',
       host: process.env.DBHOST,
@@ -22,13 +15,12 @@ import { PolygonsModel } from "./polygons/polygons.model";
       username: process.env.DBUSERNAME,
       password: process.env.DBPASSWORD,
       database: process.env.DBNAME,
-      models: [Map, OrganizationModel, PolygonsModel],
+      models: [OrganizationModel, PolygonsModel],
       autoLoadModels: true,
       define: {
         timestamps: false,
       },
     }),
-    DataModule,
     OrganizationsModule,
     PolygonsModule,
   ],
